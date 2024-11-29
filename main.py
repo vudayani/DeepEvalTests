2c2aebd3ed3ed5943ea646de8cc924efc39436ca
vudayani <vudayani@vmware.com>
2024-11-29 10:08:39 +0530

    Refactor evaluate endpoint to take custom inputs for evaluation

--- main.py
+++ main.py
@@ -10,30 +10,43 @@ app = FastAPI()
 class TestData(BaseModel):
     input: str
     actual_output: str
-    expected_output: str
+    expected_output: Optional[str] = None 
+    context: Optional[str] = None
+    retrieval_context: Optional[list[str]] = None
+    evaluation_criteria: Optional[list[str]] = None
 
 @app.post("/evaluate/")
-async def evaluate_test_case(data: TestData):
+async def evaluate_llm_response(data: TestData):
     try:
+        user_fields = data.dict(exclude_none=True).keys()
+        evaluation_params = [
+            param
+            for param in LLMTestCaseParams
+            if param.value in user_fields
+        ]
+
+
         correctness_metric = GEval(
             name="Correctness",
-            evaluation_steps=[
+            evaluation_steps=data.evaluation_criteria or [
                 "Check whether 'actual output' has all the files or code snippets (Controller, service, entity, repository) from 'expected output'",
                 "The entity class import statements should use 'jakarta.persistence' instead of 'javax.persistence'",
                 "Check if the correct dependencies are added in the 'pom.xml' file",
             ],
-            evaluation_params=[LLMTestCaseParams.INPUT, LLMTestCaseParams.ACTUAL_OUTPUT],
+            evaluation_params=evaluation_params,
             threshold=0.8
         )
 
         test_case = LLMTestCase(
             input=data.input,
             actual_output=data.actual_output,
-            expected_output=data.expected_output
+            expected_output=data.expected_output,
+            context=data.context,
+            retrieval_context=data.retrieval_context
         )
 
         correctness_metric.measure(test_case)
-        assert_test(test_case, [correctness_metric])
+        # assert_test(test_case, [correctness_metric])
 
         result = {
             "score": correctness_metric.score,
@@ -46,7 +59,3 @@ async def evaluate_test_case(data: TestData):
     except Exception as e:
         print("Error:", str(e))
         raise HTTPException(status_code=500, detail=str(e))
-
-@app.get("/test")
-async def evaluate_test_case():
-    return "Hello world!!"
